a67e5bf5894e774d0951123ce1a9fa01aa928a7e
Javed Hassan Sabu <50239488+javedhassans@users.noreply.github.com>
2021-05-02 19:01:47 +0200

    Update dash_app.py

--- dash_app.py
+++ dash_app.py
@@ -1,136 +1,77 @@
 import dash
-from dash.dependencies import Input, Output
-import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Output, Input
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
-# -------------------------------------------------------------------------------------
-# Import the cleaned data (importing csv into pandas)
-df = pd.read_csv("combined_csv.csv")
-# df = df[df['year'] == 2019]
+# Working with Data Frame
+dff = pd.read_csv("https://raw.githubusercontent.com/javedhassans/demoapp/main/workingsheet.csv")
 
-# Creating an ID column name gives us more interactive capabilities
-df['id'] = df['state']
-df.set_index('id', inplace=True, drop=False)
-print(df.columns)
+# Creating dash app
 
-# ------------------------------------------------------------------------------------
-# App layout
-app = dash.Dash(__name__, prevent_initial_callbacks=True)  # this was introduced in Dash version 1.12.0
+app = dash.Dash(__name__)
+# server = app.server
 
-# Sorting operators (https://dash.plotly.com/datatable/filtering)
 app.layout = html.Div([
-    dash_table.DataTable(
-        id='datatable-interactivity',
-        columns=[
-            {"name": i, "id": i, "deletable": True, "selectable": True, "hideable": True}
-            if i == "state"
-            else {"name": i, "id": i, "deletable": True, "selectable": True}
-            for i in df.columns
-        ],
+    html.H1('Funding on Dutch Elections',
+            style={'textAlign': 'center', 'margin-bottom': '5px', 'color': 'white'}),
+    html.H5('Amount spent by parties', style={'textAlign': 'center', 'margin-bottom': '10px', 'color': 'white'}
+            ),
 
-        data=df.to_dict('records'),  # the contents of the table
-        editable=True,  # allow editing of data inside all cells
-        filter_action="native",  # allow filtering of data by user ('native') or not ('none')
-        sort_action="native",  # enables data to be sorted per-column by user or not ('none')
-        sort_mode="single",  # sort across 'multi' or 'single' columns
-        column_selectable="multi",  # allow users to select 'multi' or 'single' columns
-        row_selectable="multi",  # allow users to select 'multi' or 'single' rows
-        row_deletable=True,  # choose if user can delete a row (True) or not (False)
-        selected_columns=[],  # ids of columns that user selects
-        selected_rows=[],  # indices of rows that user selects
-        page_action="native",  # all data is passed to the table up-front or not ('none')
-        page_current=0,  # page number that user is on
-        page_size=10,  # number of rows visible per page
-        style_cell={  # ensure adequate header width when text is shorter than cell's text
-            'minWidth': 95, 'maxWidth': 95, 'width': 95
-        },
-        style_cell_conditional=[  # align text columns to left. By default they are aligned to right
-            {
-                'if': {'column_id': c},
-                'textAlign': 'left'
-            } for c in ['Page Name', 'state']
-        ],
-        style_data={  # overflow cells' content into multiple lines
-            'whiteSpace': 'normal',
-            'height': 'auto'
-        }
-    ),
+    html.Div([
 
-    html.Br(),
-    # html.Br(),
-    html.Div(id='bar-container'),
-    # html.Div(id='choromap-container')
+        html.Div([
+            html.H6(children='Total Parties',
+                    style={'text-align': 'center',
+                           'color': 'white'}),
+            html.P(f"{dff['Party'].nunique():,.0f}",
+                   style={'text-align': 'center', 'color': 'orange',
+                          'fontSize': 40})
+
+        ], className='card_container four columns'),
+
+        html.Div([
+            html.H6(children='Total Number of Adds',
+                    style={'text-align': 'center',
+                           'color': 'white'}),
+            html.P(f"{dff['Number of Ads in Library'].sum():,.0f}",
+                   style={'text-align': 'center', 'color': 'orange',
+                          'fontSize': 40})
+
+        ], className='card_container four columns'),
+
+        html.Div([
+            html.H6(children='Total Amount in Euros',
+                    style={'text-align': 'center',
+                           'color': 'white'}),
+            html.P(f"{dff['Amount Spent (EUR)'].sum():,.0f}",
+                   style={'text-align': 'center', 'color': 'orange',
+                          'fontSize': 40})
+
+        ], className='card_container four columns'),
+
+    ], className='row flex display'),
+
+    html.Div([
+        html.Div([
+            html.P('Select Party:', className='fix_label', style={'color': 'white'}),
+            dcc.Dropdown(id='w_parties',
+                         multi=False,
+                         searchable=True,
+                         value='',
+                         placeholder='Select Party',
+                         options=[{'label':c, 'value':c}
+                                  for c in (dff['Party'].unique())], className='dcc_compon')
+
+        ], className='create_container three columns')
+
+    ], className='row flex-display')
 
 ])
 
-
-# -------------------------------------------------------------------------------------
-# Create bar chart
-
-@app.callback(
-    Output(component_id='bar-container', component_property='children'),
-    [Input(component_id='datatable-interactivity', component_property="derived_virtual_data"),
-     Input(component_id='datatable-interactivity', component_property='derived_virtual_selected_rows'),
-     Input(component_id='datatable-interactivity', component_property='derived_virtual_selected_row_ids'),
-     Input(component_id='datatable-interactivity', component_property='selected_rows'),
-     Input(component_id='datatable-interactivity', component_property='derived_virtual_indices'),
-     Input(component_id='datatable-interactivity', component_property='derived_virtual_row_ids'),
-     Input(component_id='datatable-interactivity', component_property='active_cell'),
-     Input(component_id='datatable-interactivity', component_property='selected_cells')]
-)
-def update_bar(all_rows_data, slctd_row_indices, slct_rows_names, slctd_rows,
-               order_of_rows_indices, order_of_rows_names, actv_cell, slctd_cell):
-    print('***************************************************************************')
-    print('Data across all pages pre or post filtering: {}'.format(all_rows_data))
-    print('---------------------------------------------')
-    print("Indices of selected rows if part of table after filtering:{}".format(slctd_row_indices))
-    print("Names of selected rows if part of table after filtering: {}".format(slct_rows_names))
-    print("Indices of selected rows regardless of filtering results: {}".format(slctd_rows))
-    print('---------------------------------------------')
-    print("Indices of all rows pre or post filtering: {}".format(order_of_rows_indices))
-    print("Names of all rows pre or post filtering: {}".format(order_of_rows_names))
-    print("---------------------------------------------")
-    print("Complete data of active cell: {}".format(actv_cell))
-    print("Complete data of all selected cells: {}".format(slctd_cell))
-
-    dff = pd.DataFrame(all_rows_data)
-
-    # used to highlight selected countries on bar chart
-    colors = ['#7FDBFF' if i in slctd_row_indices else '#0074D9'
-              for i in range(len(dff))]
-
-    if "Page Name" in dff and "Amount Spent (EUR)" in dff:
-        return [
-            dcc.Graph(id='bar-chart',
-                      figure=px.bar(
-                          data_frame=dff,
-                          x="Page Name",
-                          y='Amount Spent (EUR)',
-                          labels={"Euros spent on advertisement by each page"}
-                      ).update_layout(showlegend=False, xaxis={'categoryorder': 'total descending'})
-                      .update_traces(marker_color=colors, hovertemplate="<b>%{y}%</b><extra></extra>")
-                      )
-        ]
-
-
-# -------------------------------------------------------------------------------------
-# Highlight selected column
-@app.callback(
-    Output('datatable-interactivity', 'style_data_conditional'),
-    [Input('datatable-interactivity', 'selected_columns')]
-)
-def update_styles(selected_columns):
-    return [{
-        'if': {'column_id': i},
-        'background_color': '#D2F3FF'
-    } for i in selected_columns]
-
-
-# -------------------------------------------------------------------------------------
-
+# id='mainContainer', style={'display': 'flex', 'flex-direction': 'column', 'margin-bottom': '25px'}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
